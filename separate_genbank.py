7fe8baf30cc88c25cfb9584987308ebfe43e0323
Jin Choi <jinchoi721@gmail.com>
2016-05-05 14:56:14 -0500

    update

---
+++ separate_genbank.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# this script seperate genbank file
+# usage: python separate_genbank_file.py input
+
+import sys
+def write_file(cont):
+    filename = cont[0].split(" ")[7]+".gbk"
+    fwrite = open(filename,'w')
+    for line in cont:
+        fwrite.write(line)
+    fwrite.close()
+
+def main():
+    cont = []
+    fread = open(sys.argv[1],'r')
+    for line in fread:
+        if (line[:2] == '//'):
+            cont.append(line)
+            write_file(cont)
+            cont = []
+        cont.append(line)
+
+if __name__ == '__main__':
+    main()
