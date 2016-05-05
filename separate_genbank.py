0d3b2b91bb090377f66d363a59078400f87ba65e
Jin Choi <jinchoi721@gmail.com>
2016-05-05 15:11:17 -0500

    update

--- separate_genbank.py
+++ separate_genbank.py
@@ -4,7 +4,7 @@
 
 import sys
 def write_file(cont):
-    print cont[0].split(" ")
+    print cont[0]
     filename = cont[0].split(" ")[7]+".gbk"
     fwrite = open(filename,'w')
     for line in cont:
